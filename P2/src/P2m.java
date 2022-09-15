6d22b71f7205ac91904f7b3ca4ab5c870f752b54
Jorge_González-dev <jgp19112003@gmail.com>
2022-09-15 14:37:28 +0200

    .

---
+++ P2/src/P2m.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class P2m {
+
+        public static void main(String[] args) throws IOException {
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.start();
+
+    }
+}
