e4ce2277cc9db276a3a0b3972e01496e94d51e26
Jorge_González-dev <jgp19112003@gmail.com>
2022-09-15 14:31:27 +0200

    .

---
+++ P2/src/P2.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class P2 {
+
+    public static void main(String[] args) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder(args);
+        pb.start();
+    }
+}
